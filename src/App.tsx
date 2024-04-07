cae97e1caa08a227bf034fc7410f3fe20d3b4384
Kevin_Isaza <kevin.isaza.arias@hva.nl>
2024-04-07 19:55:45 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ export default function App() {
 					<div className="block sm:hidden mb-10 bg-yellow-200 py-3  text-center text-xl text-gray-700">
 						<p>For better experience please try this with pc!</p>
 						<p>This is not design for mobile view.</p>
+						<p>Thank you</p>
+
 					</div>
 					<TerminalBox />
 				</div>
