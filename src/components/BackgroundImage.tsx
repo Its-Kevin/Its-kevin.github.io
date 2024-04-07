5c7e4857e16fe1e11f83c5393f45202d17abfe3e
Kevin_Isaza <kevin.isaza.arias@hva.nl>
2024-04-07 19:35:10 +0200

    updates

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -5,7 +5,7 @@ export default function BackgroundImage() {
 	return (
 		<div className="absolute top-0 w-full h-screen -z-10">
 			<LazyLoadImage
-				src="/wallpaper.jpg"
+				src="https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 				alt="wallpaper"
 				className="h-full w-full object-cover object-center"
 				effect="blur"
